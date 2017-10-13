fb2cccc5bd087e12c358aa128db70274dd9f49d0
IrynaEngels <engelsiryna@gmail.com>
2017-10-13 20:36:22 +0300

    first

--- app/src/main/java/home/rxjavatest/TrackingGuideFragment.java
+++ app/src/main/java/home/rxjavatest/TrackingGuideFragment.java
@@ -122,9 +122,6 @@ public class TrackingGuideFragment extends Fragment {
             marker.remove();
             return true;});
     }
-    private void removeMarker(){
-
-    }
 
     private long markerId(){
         List<MyMarker> list = realmReminder.readeReminders(getActivity());
